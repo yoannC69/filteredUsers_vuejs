d90e447ad27bb48bfa090eb455acc98036b489b8
Yoann <yo.cote69@gmail.com>
2021-02-01 21:32:21 +0100

    sort by age but the view-list is not working and the arrows does not adapt to the situation

--- src/App.vue
+++ src/App.vue
@@ -22,39 +22,76 @@
     >
     Hommes</label>
 
-    <input type="text" v-model="search" placeholder="search users"/>
+  <input  
+    v-model="search"
+    type="text"
+    placeholder="search users"
+  >
 
-  <Users :usersFiltered="usersFiltered" />
+  <Users :users-filtered="usersFiltered" />
   
-        <table
-      id="tbl-users"
-      class="table table-hover"
+  <table
+    id="tbl-users"
+    class="table table-hover"
+  >
+    <thead>
+      <tr>
+        <th>image</th>
+        <th>Nom</th>
+        <th>Email</th>
+        <th>Tel</th>
+        <th>Genre</th>
+        <th>
+          Age
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            @click="sortLowest"
+          >
+            <path d="M3 3a1 1 0 000 2h11a1 1 0 100-2H3zM3 7a1 1 0 000 2h5a1 1 0 000-2H3zM3 11a1 1 0 100 2h4a1 1 0 100-2H3zM13 16a1 1 0 102 0v-5.586l1.293 1.293a1 1 0 001.414-1.414l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 101.414 1.414L13 10.414V16z" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            @click="noOrder"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            @click="sortHighest"
+          >   
+            <path d="M3 3a1 1 0 000 2h11a1 1 0 100-2H3zM3 7a1 1 0 000 2h7a1 1 0 100-2H3zM3 11a1 1 0 100 2h4a1 1 0 100-2H3zM15 8a1 1 0 10-2 0v5.586l-1.293-1.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L15 13.586V8z" />
+          </svg>
+        </th>
+      </tr>
+    </thead>
+    <tbody
+      v-if="users"
+      id="tbody-users"
     >
-      <thead>
-        <tr>
-          <th>image</th>
-          <th>Nom</th>
-          <th>Email</th>
-          <th>Tel</th>
-          <th>Genre</th>
-        </tr>
-      </thead>
-      <tbody id="tbody-users" v-if="users">
-        <tr
-          v-for="user in searchedUsers"
-          :key="user.login.uuid"
-        >
-          <td><img :src="user.picture.thumbnail"></td>
-          <td>{{ user.name.first }} {{ user.name.last }}</td>
-          <td>{{ user.email }}</td>
-          <td>{{ user.phone }}</td>
-          <td>{{ user.gender }}</td>
-          <td />
-        </tr>
-      </tbody>
-    </table>
-
-
+      <tr
+        v-for="user in searchedUsers" 
+        :key="user.login.uuid"
+      >
+        <td><img :src="user.picture.thumbnail"></td>
+        <td>{{ user.name.first }} {{ user.name.last }}</td>
+        <td>{{ user.email }}</td>
+        <td>{{ user.phone }}</td>
+        <td>{{ user.gender }}</td>
+        <td>{{ user.dob.age }}</td>
+        <td />
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
@@ -84,6 +121,8 @@ export default {
       });
     },
   },
+    
+
 
   methods: {
     fetchUsers() {
@@ -96,7 +135,16 @@ export default {
             console.log(error)
             this.errored = true
           })
-    }
+    },
+    sortLowest() {
+      this.users.sort((a, b) => a.dob.age > b.dob.age ? 1 : -1);
+    },
+    sortHighest() {
+      this.users.sort((a, b) => a.dob.age < b.dob.age ? 1 : -1);
+    },
+    noOrder() {
+      this.users.sort((a, b) => a.dob.age == b.dob.age ? 1 : -1);
+    },
   },
 
 
