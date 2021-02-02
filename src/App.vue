fba478c50e8f31136d84d4c8f18e0d248236cb2e
Yoann <yo.cote69@gmail.com>
2021-02-02 15:02:59 +0100

    correction

--- src/App.vue
+++ src/App.vue
@@ -3,84 +3,86 @@
     alt="Vue logo"
     src="./assets/logo.png"
   >
-  
-  <button @click="fetchUsers">
-    fetch users
-  </button>
-  <label>
-    <input
-      v-model="genderFilter"
-      type="checkbox"
-      value="female"
+  <DispUsers msg="Welcome to Your Vue.js App" />
+  <div class="headt">
+    <button
+      class="btn btn-primary"
+      @click="fetchUsers"
     >
-    Femmes</label>
-  <label>
-    <input
-      v-model="genderFilter"
-      type="checkbox"
-      value="male"
-    >
-    Hommes</label>
-
-  <input  
-    v-model="search"
-    type="text"
-    placeholder="search users"
-  >
-
-  <Users :users-filtered="usersFiltered" />
-  
+      Réupérer des utilisateurs
+    </button>
+    <label>
+      <input
+        v-model="genderFilter"
+        type="checkbox"
+        value="male"
+      >
+      Hommes
+    </label>
+    <label>
+      <input
+        v-model="genderFilter"
+        type="checkbox"
+        value="female"
+      >
+      Femmes
+    </label>
+    <label>
+      Rechercher :
+      <input
+        v-model="search"
+        type="text"
+        placeholder="Rechercher"
+      >
+    </label>  
+    <label>Trier par âge :          
+    </label>
+    <p v-if="sortDirection === ''">
+      Par défaut
+    </p>
+    <p v-if="sortDirection === 'asc'">
+      Croissant
+    </p>
+    <p v-if="sortDirection === 'desc'">
+      Décroissant
+    </p>
+  </div>
+  <p v-if="users.length">
+    il y a <strong>{{ searchedUsers.length }}</strong> utilisateurs
+  </p>
+  <p v-else>
+    il n'y a <strong>aucun</strong> utilisateur
+  </p>
   <table
-    id="tbl-users"
+    v-if="users.length"
     class="table table-hover"
   >
     <thead>
       <tr>
-        <th>image</th>
+        <th>Photo</th>
         <th>Nom</th>
         <th>Email</th>
         <th>Tel</th>
         <th>Genre</th>
         <th>
-          Age
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            @click="sortLowest"
+          <button
+            class="btn btn-light"
+            @click="changeSort"
           >
-            <path d="M3 3a1 1 0 000 2h11a1 1 0 100-2H3zM3 7a1 1 0 000 2h5a1 1 0 000-2H3zM3 11a1 1 0 100 2h4a1 1 0 100-2H3zM13 16a1 1 0 102 0v-5.586l1.293 1.293a1 1 0 001.414-1.414l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 101.414 1.414L13 10.414V16z" />
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            @click="noOrder"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
+            Âge
+            <i
+              v-if="sortDirection"
+              class="fa"
+              :class="[ sortDirection === 'asc' ? 'fa-sort-up' : 'fa-sort-down' ]"
             />
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            @click="sortHighest"
-          >   
-            <path d="M3 3a1 1 0 000 2h11a1 1 0 100-2H3zM3 7a1 1 0 000 2h7a1 1 0 100-2H3zM3 11a1 1 0 100 2h4a1 1 0 100-2H3zM15 8a1 1 0 10-2 0v5.586l-1.293-1.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L15 13.586V8z" />
-          </svg>
+          </button>
         </th>
       </tr>
     </thead>
-    <tbody
-      v-if="users"
-      id="tbody-users"
-    >
+    <tbody>
       <tr
-        v-for="user in searchedUsers" 
-        :key="user.login.uuid"
+        v-for="user in searchedUsers"
+        :key="user.email"
       >
         <td><img :src="user.picture.thumbnail"></td>
         <td>{{ user.name.first }} {{ user.name.last }}</td>
@@ -88,66 +90,69 @@
         <td>{{ user.phone }}</td>
         <td>{{ user.gender }}</td>
         <td>{{ user.dob.age }}</td>
-        <td />
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
-import Users from './components/Users.vue'
 import axios from 'axios'
-
 export default {
   name: 'App',
   components: {
-    Users
   },
-  data(){
+  data() {
     return {
       users: [],
-      genderFilter: ['male','female'],
-      search:''
+      errored: false,
+      genderFilter: ['male', 'female'],
+      search: '',
+      sortDirection: ''
     }
   },
-
   computed: {
-    usersFiltered() {
-      return this.users.filter((user) => this.genderFilter.includes(user.gender))
-    },
     searchedUsers() {
-      return this.usersFiltered.filter((user) => {
-        return user.name.first.toLowerCase().match(this.search.toLowerCase());
-      });
+      return this.users
+      .filter((user) => this.genderFilter.includes(user.gender))
+      .filter((user) => {
+        return (user.name.first.toLowerCase().includes(this.search.toLowerCase())) ||
+        (user.name.last.toLowerCase().includes(this.search.toLowerCase()))
+      })
+      .sort((a,b) => {
+        if (!this.sortDirection) return 0;
+        const  modifier = this.sortDirection === 'desc' ? -1 : 1;
+        return (a.dob.age - b.dob.age) * modifier;
+      })
     },
   },
-    
-
-
+  created(){
+    this.fetchUsers()},
+  
   methods: {
-    fetchUsers() {
+    fetchUsers() { 
       axios
         .get('https://randomuser.me/api/?results=20')
         .then(response => {
-          this.users.push.apply(this.users,response.data.results)
+         this.users = [...this.users, ...response.data.results]
+         //this.users = this.users.concat(response.data.results)
         })
         .catch(error => {
-            console.log(error)
-            this.errored = true
-          })
+          console.error(error)
+          this.errored = true
+        })
     },
-    sortLowest() {
-      this.users.sort((a, b) => a.dob.age > b.dob.age ? 1 : -1);
-    },
-    sortHighest() {
-      this.users.sort((a, b) => a.dob.age < b.dob.age ? 1 : -1);
-    },
-    noOrder() {
-      this.users.sort((a, b) => a.dob.age == b.dob.age ? 1 : -1);
+    changeSort() {
+      if (this.sortDirection === ''){
+        this.sortDirection = 'asc'
+        return this.users
+      }else if (this.sortDirection === 'asc'){
+        this.sortDirection = 'desc'
+      } else if (this.sortDirection === 'desc'){
+        this.sortDirection = ''
+      }
     },
   },
-
-
+  
 }
 </script>
 
@@ -159,5 +164,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.headt{
+  display: flex;
+  justify-content: space-around;
+}
+.btn-primary{
+  background-color: #41B883!important;
+  border-color: #41B883!important;
+}
+.btn-primary:hover{
+  background-color: #35495E!important;
 }
 </style>
